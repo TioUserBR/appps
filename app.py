28d8d3286773e173dd841b113d18689f5eb45122
TioUserBR <tteulina@gmail.com>
2026-01-29 21:46:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -253,6 +253,6 @@ def api_estatisticas():
 # Criar tabelas
 with app.app_context():
     db.create_all()
-
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
